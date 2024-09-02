887b517ee9a67b45c3e14c898a09bddad9a4284e
Maciej Wójcik <w8jcik@gmail.com>
2024-09-02 13:18:34 +0200

    Remove nesting

--- dist.sh
+++ dist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf dist
-mkdir -p dist/display-adjustment@w8jcik.gitlab.com
+mkdir -p dist/
 
-cp *.js COPYING metadata.json README.md screenshot.png dist/display-adjustment@w8jcik.gitlab.com/
+cp *.js COPYING metadata.json README.md screenshot.png dist/
