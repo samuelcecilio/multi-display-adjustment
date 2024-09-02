c1e5fef2ea14e5ab42a880d7227a696b7cd91fd3
Maciej Wójcik <w8jcik@gmail.com>
2024-09-02 13:18:08 +0200

    Distribution script

--- dist.sh
+++ dist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf dist
-mkdir dist
+mkdir -p dist/display-adjustment@w8jcik.gitlab.com
 
-cp *.js COPYING metadata.json README.md screenshot.png dist/
+cp *.js COPYING metadata.json README.md screenshot.png dist/display-adjustment@w8jcik.gitlab.com/
