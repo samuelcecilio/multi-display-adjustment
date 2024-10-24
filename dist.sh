fba70e3ff3b2d9226373ebdb779e730cec5693db
Maciej Wójcik <w8jcik@gmail.com>
2024-10-24 20:01:39 +0200

    Increase patch version

--- dist.sh
+++ dist.sh
@@ -3,5 +3,5 @@
 rm -rf ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com
 mkdir -p ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com
 
-rsync -a ./ --exclude dev.md --exclude .gitignore --exclude .git --exclude dist.sh ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com/
+rsync -a ./ --exclude dev.md --exclude .gitignore --exclude .git --exclude dist.sh --exclude README.md ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com/
 zip -rj dist.zip ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com
