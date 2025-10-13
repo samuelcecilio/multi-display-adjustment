904564be945ec15236eb9812d864210b630d92ee
Maciej Wójcik <w8jcik@gmail.com>
2025-10-13 17:31:57 +0200

    Exclude troubleshooting instructions from distribution

--- dist.sh
+++ dist.sh
@@ -3,6 +3,6 @@
 rm -rf ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com
 mkdir -p ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com
 
-rsync -a ./ --exclude dev.md --exclude .gitignore --exclude .git --exclude dist.sh --exclude README.md ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com/
+rsync -a ./ --exclude dev.md --exclude .gitignore --exclude .git --exclude dist.sh --exclude README.md --exclude troubleshooting.md ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com/
 rm -f dist.zip
 zip -rj dist.zip ~/.local/share/gnome-shell/extensions/display-adjustment@w8jcik.gitlab.com
